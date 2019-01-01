15e39f6a31e381c3297c0469af3eaf1f370ccd9f
richardfuller <rcf799+github@gmail.com>
2018-12-31 21:57:49 -0500

    initial commit

---
+++ DrugShortagesAPI/Controllers/ApiController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DrugShortagesAPI;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DrugShortagesAPI.Controllers
+{
+    [Route("api/shortages")]
+    public class ShortageController : Controller
+    {
+        private string ShortageURL = "https://www.ashp.org/rss/shortages/";
+        // GET: api/values
+        [HttpGet]
+        public async Task<List<RssParser.FeedItem>> GetAsync()
+        {
+            RssParser shortages = new RssParser(); 
+            return await shortages.GetListAsync(ShortageURL);
+        }
+
+    }
+
+    [Route("api/discontinued")]
+    public class UnavilableController : Controller
+    {
+        private string UnavailableURL = "https://www.ashp.org/rss/notavailable/";
+        [HttpGet]
+        public async Task<List<RssParser.FeedItem>> GetAsync()
+        {
+            RssParser discontinued = new RssParser();
+            return await discontinued.GetListAsync(UnavailableURL);
+        }
+    }
+
+    [Route("api/resolved")]
+    public class ResolvedController : Controller
+    {
+        private string resolvedURL = "https://www.ashp.org/rss/resolved/";
+        [HttpGet]
+        public async Task<List<RssParser.FeedItem>> GetAsync()
+        {
+            RssParser resolved = new RssParser();
+            return await resolved.GetListAsync(resolvedURL);
+        }
+    }
+
+
+    [Route("api/notavailable")]
+    public class NotAvailableController : Controller
+    {
+        private string notavailableURL = "https://www.ashp.org/rss/nopresentations";
+        [HttpGet]
+        public async Task<List<RssParser.FeedItem>> GetAsync()
+        {
+            RssParser notavailable = new RssParser();
+            return await notavailable.GetListAsync(notavailableURL);
+        }
+    }
+}
---
+++ DrugShortagesAPI/Shortages.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace DrugShortagesAPI
+{
+    public class RssParser
+    {
+
+        public async System.Threading.Tasks.Task<List<FeedItem>> GetListAsync(string feedUrl)
+        {
+            var articles = new List<FeedItem>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(feedUrl);
+                var responseMessage = await client.GetAsync(feedUrl);
+                var responseString = await responseMessage.Content.ReadAsStringAsync();
+
+                //extract feed items
+                XDocument doc = XDocument.Parse(responseString);
+                var feedItems = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
+                                select new FeedItem
+                                {
+                                    uid = item.Elements().First(i => i.Name.LocalName == "guid").Value,
+                                    mainText = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                                    redirectionUrl = item.Elements().First(i => i.Name.LocalName == "link").Value,
+                                    updateDate = DateTime.Parse(item.Elements().First(i => i.Name.LocalName == "pubDate").Value).ToUniversalTime(),
+                                    titleText = item.Elements().First(i => i.Name.LocalName == "title").Value
+                                };
+                articles = feedItems.ToList().FindAll(i => i.updateDate >= DateTime.Today.AddDays(-7));
+                return articles;
+            }
+        }
+
+        public class FeedItem
+        {
+            public string uid { get; set; }
+            public DateTime updateDate { get; set; }
+            public string titleText { get; set; }
+            public string mainText { get; set; }
+            public string redirectionUrl { get; set; }
+
+
+        }
+    }
+}
